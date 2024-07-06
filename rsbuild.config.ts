411973cb858efdaa7c4977621706bc8c269b28b5
nrmnqdds <quddusnuriman@gmail.com>
2024-07-06 22:29:52 +0800

    :tada: init commit

---
+++ rsbuild.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from "@rsbuild/core";
+import { pluginImageCompress } from "@rsbuild/plugin-image-compress";
+import { pluginNodePolyfill } from "@rsbuild/plugin-node-polyfill";
+import { pluginReact } from "@rsbuild/plugin-react";
+import { pluginTypeCheck } from "@rsbuild/plugin-type-check";
+
+export default defineConfig({
+	plugins: [
+		pluginReact(),
+		pluginTypeCheck(),
+		pluginImageCompress(),
+		pluginNodePolyfill(),
+	],
+	html: {
+		template: "./index.html",
+	},
+	source: {
+		entry: {
+			index: "./src/main.tsx",
+		},
+	},
+});
